2b88e9f5c1d047fabfe68146fd7c187b586b70d3
Corentin Rejaud <crejaud@nbp-140-45.nbp.ruw.rutgers.edu>
2017-02-05 16:59:51 -0500

    added test 10m file and all works

--- p05.c
+++ p05.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h> // for syscmp and strstr
+#include <ctype.h> // for tolower
+#include <fcntl.h> // for syscall open
+#include <unistd.h> // for syscall close
 #include "p05.h"
+
+#define TRUE 1
+#define FALSE 0
 
 static const char *SYSTEM_CALL_FLAG = "--systemcalls";
 
@@ -14,21 +20,22 @@ int main(int argc, char *argv[]) {
 		exit(1);	
 	}
 
-	bool should_use_systemcalls = false;
+	/* should_use_systemcalls is a boolean */
+	int should_use_systemcalls = FALSE;
 	int arg_index;
 	for (arg_index = 2; arg_index < argc; arg_index++) {
 		if (strcmp(SYSTEM_CALL_FLAG, argv[arg_index]) == 0) {
 			/* if there is only the file and the flag, then exit */
-			if (argv == 2) {
+			if (argc == 2) {
 				exit(1);
 			}
-			should_use_systemcalls = true;
+			should_use_systemcalls = TRUE;
 		}
 	}
 
 	char *buffer;
 
-	if (should_use_systemcalls) {
+	if (should_use_systemcalls == TRUE) {
 		buffer = get_buffer_with_syscalls(argv);	
 	}
 	else {
@@ -40,20 +47,18 @@ int main(int argc, char *argv[]) {
 	//printf("%s\n", buffer);
 
 	/* Get number of occurrences of each substring inside the buffer */
-	int arg_index;
 	for (arg_index = 2; arg_index < argc; arg_index++) {
 		/* if this is the system call flag, then skip this argument */
-		if (strcmp(SYSTEM_CALL_FLAG, argv[arg_idnex]) == ) {
+		if (strcmp(SYSTEM_CALL_FLAG, argv[arg_index]) == 0) {
 			continue;
 		}
 		to_lower_case(argv[arg_index]);
 		//printf("%s\n", argv[arg_index]);
-		int num_occurrences = get_num_occurrences_of_substring_in_buffer(buffer, file_size, argv[arg_index]);
+		int num_occurrences = get_num_occurrences_of_substring_in_buffer(buffer, argv[arg_index]);
 		printf("%d\n", num_occurrences);
 	}  
 
-	/* Close file */
-	fclose(fp);
+	free(buffer);
 
 	return 0;
 }
@@ -61,15 +66,30 @@ int main(int argc, char *argv[]) {
 /* Get buffer with system calls */
 char* get_buffer_with_syscalls(char *argv[]) {
 	/* Open file descriptor from 2nd argument */
-	int input_fd = open(argv[1], O_RDONLY);
+	int fd = open(argv[1], O_RDONLY);
 	
 	/* If file descriptor wasn't found */
-	if (input_fd == -1) {
+	if (fd == -1) {
 		perror("open (syscalls)");
 		exit(1);
 	}
 
+	/* Get size of file */
+	int file_size = get_size_of_file_descriptor(fd);	 
+
+	/* Read file and store in buffer of size file_size by character */
+	char *buffer = (char *) malloc(file_size * sizeof(char));
 	
+	/* Memory allocation failure */
+	if (!buffer) {
+		exit(1);
+	}
+	read_file_descriptor_and_store_in_buffer(fd, file_size, buffer);
+
+	/* Close file descriptor */
+	close(fd);
+
+	return buffer;
 }
 
 /* Get buffer without system calls */
@@ -88,8 +108,17 @@ char* get_buffer_without_syscalls(char *argv[]) {
 
 	/* Read file and store in buffer of size file_size */
 	char *buffer = (char *) malloc(file_size * sizeof(char));
+	
+	/* Memory allocation failure */
+	if (!buffer) {
+		exit(1);
+	}
+	
 	read_file_and_store_in_buffer(fp, file_size, buffer);
 
+	/* Close file */
+	fclose(fp);
+	
 	return buffer;
 }
 
@@ -104,11 +133,32 @@ int get_size_of_file(FILE *fp) {
 	return file_size;
 }
 
+/* Get the size of the file descriptor by seeking to the end,
+ * getting the position,
+ * then rewinding the file descriptor back to the start
+*/
+int get_size_of_file_descriptor(int fd) {
+	int file_size = lseek(fd, 0, SEEK_END);
+	lseek(fd, 0, SEEK_SET);
+	return file_size;
+}
+
+/* Read the file all at once using fread */
 void read_file_and_store_in_buffer(FILE *fp, int file_size, char *buffer) {
 	int end_pos = fread((void *) buffer, 1, file_size - 1, fp);
 	/* set end of buffer at end_pos */
 	//printf("End position: %d\n", end_pos);
 	buffer[end_pos] = 0;
+}
+
+/* Read the file character by character using read */
+void read_file_descriptor_and_store_in_buffer(int fd, int file_size, char *buffer) {
+	char c;
+	int counter;
+	for (counter = 0; read(fd, &c, 1) == 1; counter++) {
+		buffer[counter] = c;
+	}
+	//printf("%s\n", buffer);
 }
 
 void to_lower_case(char *string) {
@@ -118,7 +168,7 @@ void to_lower_case(char *string) {
 	}
 }
 
-int get_num_occurrences_of_substring_in_buffer(char *buffer, int file_size, char *substring) {
+int get_num_occurrences_of_substring_in_buffer(char *buffer, char *substring) {
 	int num_occurrences = 0;
 	char *haystack = buffer;
 	//printf("haystack: %s\n", haystack);
--- p05.h
+++ p05.h
@@ -1,4 +1,8 @@
+char* get_buffer_with_syscalls(char *argv[]);
+char* get_buffer_without_syscalls(char *argv[]);
 int get_size_of_file(FILE *fp);
+int get_size_of_file_descriptor(int fd);
 void read_file_and_store_in_buffer(FILE *fp, int file_size, char *buffer);
+void read_file_descriptor_and_store_in_buffer(int fp, int file_size, char *buffer);
 void to_lower_case(char *string);
-int get_num_occurrences_of_substring_in_buffer(char *buffer, int file_size, char *substring);
+int get_num_occurrences_of_substring_in_buffer(char *buffer, char *substring);
